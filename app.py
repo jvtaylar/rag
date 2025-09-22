b89d0ecd25f6695b00bb20506870b012eb48b75f
jvtaylar <45677039+jvtaylar@users.noreply.github.com>
2025-09-22 18:32:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ AZURE_OPENAI_ENDPOINT = "https://jvtay-mff428jo-eastus2.openai.azure.com/"
 AZURE_OPENAI_KEY = "FOObvelUv1Ubbw0ZlEb3NPCBYDbdXWbLhzyckQAA9cP3Ofhgi8KWJQQJ99BIACHYHv6XJ3w3AAAAACOGoHUz"
 AZURE_CHAT_DEPLOYMENT = "gpt-35-turbo"          # e.g., gpt-35-turbo
 AZURE_EMBEDDING_DEPLOYMENT = "text-embedding-ada-002"  # e.g., text-embedding-ada-002
-AZURE_API_VERSION = "2024-02-01"
+AZURE_API_VERSION = "2024-12-01-preview"
 
 # --------------------------
 # Load Documents (Sample FAQ/Manuals)
